6d68920d0168df2d0e18e9c410c1abad210ab2fc
aryanugroho <mail.anugroho@gmail.com>
2020-04-20 21:07:33 +0700

    validate input

--- cmd/antrian-bucket/app.go
+++ cmd/antrian-bucket/app.go
@@ -3,19 +3,29 @@ package main
 import (
 	"bufio"
 	"fmt"
+	"github.com/aryanugroho/antrian-bucket-go/pkg/antrian"
 	"os"
 	"strings"
 )
 
 func main() {
 	fmt.Println("how many slot of lokets you want to create?")
-
+	// get the input
 	reader := bufio.NewReader(os.Stdin)
 	input, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Printf("an error occurred while reading line, errors %s", err.Error())
+		return
 	}
 
 	inputs := strings.Split(strings.Trim(input, "\n"), " ")
+
+	// validate slot should be between 1 - 5
+	isValid := antrian.Validate(inputs[0])
+	if !isValid {
+		fmt.Printf("number of slot should be 1 - 5")
+		return
+	}
+
 	fmt.Println(inputs)
 }
