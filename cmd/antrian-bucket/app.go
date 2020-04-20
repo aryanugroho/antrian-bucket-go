e5ab23d18cfd365f0b6357e2a60c2a804c8b69a1
aryanugroho <mail.anugroho@gmail.com>
2020-04-20 20:43:14 +0700

    get number of slot from input

--- cmd/antrian-bucket/app.go
+++ cmd/antrian-bucket/app.go
@@ -1,5 +1,21 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
 
+func main() {
+	fmt.Println("how many slot of lokets you want to create?")
+
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		fmt.Printf("an error occurred while reading line, errors %s", err.Error())
+	}
+
+	inputs := strings.Split(strings.Trim(input, "\n"), " ")
+	fmt.Println(inputs)
 }
